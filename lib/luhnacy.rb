979299c53d143653032b6cf956f3ecf2a5d344bf
Rory McKinley <rorymckinley@gmail.com>
2010-07-18 10:11:34 +0200

    Added a simple method to validate Luhn algorithm - plus a pending test for the generator

---
+++ lib/luhnacy.rb
@@ -0,0 +1,22 @@
+class Luhnacy
+  def self.valid?(candidate)
+    working = candidate.reverse
+    double_up = false;
+    sum = 0
+
+    working.each_char do |ch|
+      num = ch.to_i
+      sum += double_up ? double_and_fix(num) : num
+      double_up = !double_up
+    end
+    sum % 10 == 0
+  end
+  def self.generate(string_size)
+    return "11111"
+  end
+
+  private
+  def self.double_and_fix(number)
+    2 * number > 9 ? ( (2 * number) % 10 + 1 ) : 2 * number
+  end
+end
--- spec/luhnacy_spec.rb
+++ spec/luhnacy_spec.rb
@@ -1,7 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Luhnacy" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  def luhn?(string)
+    working = string.reverse
+    double_up = false;
+    sum = 0
+
+    working.each_char do |ch|
+
+    end
+  end
+  it "should be able to identify if a number satisfies Luhn" do
+    valid_number = '49927398716'
+    Luhnacy.valid?(valid_number).should be_true
+  end
+
+  it "should be able to identify if a number does not satisfy Luhn" do
+    invalid_number = '49927398715'
+    Luhnacy.valid?(invalid_number).should be_false
+  end
+
+  it "should return a string of digits that satisfies Luhn" do
+    pending
+    string_size = 5
+    candidate = Luhnacy.generate(string_size)
+    candidate.should match /^\d{#{string_size}}$/
+    luhn?(candidate).should be_true
   end
 end
